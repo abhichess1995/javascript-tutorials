e9b036444f4f45200c90eca519eeae65daec488f
abhichess1995 <abhichess1995@gmail.com>
2026-04-07 08:56:44 +0000

    Operations update

--- basics/3_conversionOperations.js
+++ basics/3_conversionOperations.js
@@ -64,5 +64,44 @@ let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber) // 33
-console.log(typeof stringNumber) // string
+//console.log(stringNumber) // 33
+//console.log(typeof stringNumber) // string
+
+
+/************** Operations   *****************/
+
+let val = 3
+let negVal = -val
+//console.log(negVal) // -3
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/3)
+console.log(2**3)
+console.log(2%3)
+*/
+
+let str1 = "Hello"
+let str2 = " Abhi"
+let str3 = str1 + str2
+//console.log(str3) // Hello Abhi
+
+// console.log(1 + "1") // 11
+// console.log("1" + 1) // 11
+// console.log(1 + 1 + "1") // 21 
+// console.log("1" + 1 + 1); // 111 because "1" + 1 => "11" + 1 => "111"
+
+// ++x and x++ both are used to increment the value of x by 1. The difference between them is in the order of evaluation:
+
+// ++x (pre-increment): The value of x is incremented first, and then the new value is returned.
+// x++ (post-increment): The current value of x is returned first, and then x is incremented.
+
+let x = 5
+// console.log(++x) // 6
+// console.log(x) // 6
+
+let y = 5
+// console.log(y++) // 5
+// console.log(y) // 6
