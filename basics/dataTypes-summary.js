1cf652d4fdfd5a98a73315758f48ab5138c4a21b
abhichess1995 <abhichess1995@gmail.com>
2026-04-07 09:27:47 +0000

    primitive and non primitive

--- basics/dataTypes-summary.js
+++ basics/dataTypes-summary.js
@@ -26,3 +26,33 @@ function greet() {
     console.log('Hello!')
 }
 
+/******************************************************** */
+// Primitives are immutable, meaning their values cannot be changed. When you assign a primitive value to a variable, it creates a copy of that value. If you change the value of one variable, it does not affect the other variable.
+
+let a = 10
+let b = a // b is now a copy of a
+a = 20 // changing a does not affect b
+console.log(a); // 20
+console.log(b); // 10
+
+// Non-primitives are mutable, meaning their values can be changed. When you assign a non-primitive value to a variable, it creates a reference to that value. If you change the value of one variable, it affects the other variable because they both reference the same object in memory.
+
+let obj1 = { name: 'John' }
+let obj2 = obj1 // obj2 is now a reference to obj1
+obj1.name = 'Jane' // changing obj1 also changes obj2
+console.log(obj1.name); // Jane
+console.log(obj2.name); // Jane
+
+// To create a copy of a non-primitive value, you can use methods like Object.assign() for objects or the spread operator for arrays.
+
+let obj3 = { name: 'John' }
+let obj4 = Object.assign({}, obj3) // creates a copy of obj3
+obj3.name = 'Jane' // changing obj3 does not affect obj4
+console.log(obj3.name); // Jane
+console.log(obj4.name); // John
+
+let arr1 = [1, 2, 3]
+let arr2 = [...arr1] // creates a copy of arr1
+arr1.push(4) // changing arr1 does not affect arr2
+console.log(arr1); // [1, 2, 3, 4]
+console.log(arr2); // [1, 2, 3]
